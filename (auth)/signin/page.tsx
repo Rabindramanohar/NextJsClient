0f8efbf230e6fa7387b41726ed044b5ecfab7b5c
rabindra <robinmanohar@gmail.com>
2024-03-02 21:04:00 +0530

    Initial commit

---
+++ (auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+import { SigninComponent } from "@/app/components/signin";
+
+export default function Signin() {
+  return (
+    <div>
+      <SigninComponent />
+    </div>
+  );
+}
---
+++ (auth)/signup/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Signin() {
+  return <div>Inside signup!</div>;
+}
